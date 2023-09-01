63990174e2a3c8e337c739d75fe3090a1a8a0c24
up1 <somkiat.p@gmail.com>
2023-09-01 14:29:21 +0700

    Config circuit breaker

--- work2/src/main/java/com/example/work2/OrderController.java
+++ work2/src/main/java/com/example/work2/OrderController.java
@@ -1,5 +1,7 @@
 package com.example.work2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -7,9 +9,18 @@ import reactor.core.publisher.Mono;
 @RestController
 public class OrderController {
 
+    @Autowired PaymentService paymentService;
+
     @GetMapping("/process")
     public Mono<String> process() {
         return Mono.just("Hello");
+    }
+}
+
+@Service
+class PaymentService {
+
+    void pay() {
     }
 
 }
--- work2/src/main/java/com/example/work2/Work2Application.java
+++ work2/src/main/java/com/example/work2/Work2Application.java
@@ -1,13 +1,34 @@
 package com.example.work2;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class Work2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Work2Application.class, args);
+	}
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.circuitBreakerConfig(
+						CircuitBreakerConfig.custom()
+								.failureRateThreshold(10)
+								.slowCallRateThreshold(5).build()
+				)
+				.timeLimiterConfig(TimeLimiterConfig.custom()
+						.timeoutDuration(Duration.ofSeconds(3)).build()).build());
 	}
 
 }
